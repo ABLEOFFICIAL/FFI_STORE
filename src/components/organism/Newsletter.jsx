ce797274a76f667e317c786067c505c46f17bf56
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2025-06-02 08:57:40 +0800

    wip

--- src/components/organism/Newsletter.jsx
+++ src/components/organism/Newsletter.jsx
@@ -14,11 +14,11 @@ const Newsletter = () => {
         <input
           type="email"
           placeholder="Enter your email"
-          className="px-4 py-2 border border-white rounded-3xl w-full md:w-auto pr-16"
+          className="px-4 py-2 border border-white rounded-3xl w-full md:w-auto pr-16 outline-none focus:outline-none focus:ring-0"
         />
         <button
           type="submit"
-          className="bg-[#4a4741] text-xs text-white px-2 py-2 rounded-r-3xl hover:bg-[#3b342c] absolute right-0 top-0 h-full md:rounded-l-none md:rounded-r-3xl"
+          className="bg-[#4a4741] text-xs text-white px-2 py-2 rounded-r-3xl hover:bg-[#3b342c] absolute right-0 top-0 h-full md:rounded-l-none  md:rounded-r-3xl"
         >
           Subscribe
         </button>
